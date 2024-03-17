a2f93fa35fa1a786ec07d207a1ee17fc492223df
MamaneHassane <hassane.mamane017@gmail.com>
2024-03-17 16:56:38 +0000

    just starting

--- lib/pages/navigation.dart
+++ lib/pages/navigation.dart
@@ -1,22 +1,57 @@
-// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, must_be_immutable
 
 import 'package:flutter/material.dart';
 import 'package:google_nav_bar/google_nav_bar.dart';
 
-class Navigation extends StatelessWidget {
+class Navigation extends StatefulWidget {
   const Navigation({super.key});
+
+  @override
+  State<Navigation> createState() => _NavigationState();
+}
+
+class _NavigationState extends State<Navigation> {
+  // La liste des pages
+  final List pages = [
+    
+  ];
+
+  // L'index courant
+  int currentPageIndex = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      body: pages[currentPageIndex] ,
       appBar: AppBar(
         title: Text(
           'VideoGameShop'
         ),
       ) ,
       bottomNavigationBar: GNav(
+        selectedIndex: currentPageIndex,
+        onTabChange: (index){
+          setState(() {
+            currentPageIndex = index;
+          });
+        },
         tabs: [
-          
+          GButton(
+            icon: Icons.favorite,
+            text: 'Wishlist'
+          ),
+          GButton(
+            icon: Icons.home_outlined,
+            text: 'Home'
+          ),
+          GButton(
+            icon: Icons.settings,
+            text: 'Settings'
+          ),
+          GButton(
+            icon: Icons.person_outline,
+            text: 'Profile'
+          )
       ]),
     );
   }
